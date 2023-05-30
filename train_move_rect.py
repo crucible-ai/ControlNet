8ede6e153ee97968b9d4c4012a968dbe513f3a4a
Joseph Catrambone <joseph@crucible.ai>
2023-05-30 15:21:11 -0700

    Stupid bugfix: target range is not the same as the source range.  Target has to be in -1,1 while source is 0,1.  Add image logging.

--- train_move_rect.py
+++ train_move_rect.py
@@ -6,10 +6,14 @@ from typing import Optional, Tuple
 
 import numpy
 import pytorch_lightning as pl
+import torch
+import torchvision
 from PIL import Image, ImageDraw
+from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+from cldm.ddim_hacked import DDIMSampler
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
 from share import *
@@ -64,7 +68,7 @@ class MoveRectDataset(Dataset):
 
         # Normalize source images [0, 1].
         source = source.astype(numpy.float32) / 255.0
-        target = target.astype(numpy.float32) / 255.0
+        target = (target.astype(numpy.float32) / 127.5) - 1.0
 
         return dict(jpg=target, txt=random.choice(["an image", "a picture", ".", " "]), hint=source)
 
@@ -126,18 +130,66 @@ model.only_mid_control = only_mid_control
 # Save every so often:
 ckpt_callback = pl.callbacks.ModelCheckpoint(
         dirpath="./checkpoints/",
-        filename="controlnet_sd15_moverect_face_{epoch}_{step}_{loss_simple_step}.ckpt",
+        filename="controlnet_sd15_moverect_face_{epoch}_{step}_{loss_simple_step}",
         monitor='train/loss_simple_step',
         save_top_k=5,
         every_n_train_steps=5000,
         save_last=True,
 )
 
+
+# Make a logging callback to store a sample image to the tensorboard.
+class TensorboardDiffusionImageSampler(pl.callbacks.Callback):
+    def __init__(self, ds: MoveRectDataset) -> None:
+        super().__init__()
+        self.ds = ds
+
+    def on_train_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        #dim = (self.num_samples, pl_module.hparams.latent_dim)
+        #z = torch.normal(mean=0.0, std=1.0, size=dim, device=pl_module.device)
+
+        example_dict = self.ds[random.randint(0, len(self.ds)-1)]
+        with torch.no_grad():
+            pl_module.eval()
+            single_image = TensorboardDiffusionImageSampler.run_inference(
+                model=pl_module,
+                image_tensor=example_dict['hint'].unsqueeze(0)  # Batch of '1'.
+            )
+            pl_module.train()
+        source_prediction_expected = torch.stack([example_dict['hint'], single_image.squeeze(0), example_dict['jpg']], dim=0)
+
+        grid = torchvision.utils.make_grid(
+            tensor=source_prediction_expected,
+            nrow=3,
+            padding=2,
+            normalize=True,
+            scale_each=True,  # Scale all images to 0,1 individually, not together.
+        )
+        str_title = f"{pl_module.__class__.__name__}_images"
+        trainer.logger.experiment.add_image(str_title, grid, global_step=trainer.global_step)
+
+    @staticmethod
+    def run_inference(model, image_tensor, prompt, n_prompt: str = "", ddim_steps: int = 20, guidance_scale: float = 9.0):
+        sampler = DDIMSampler(model)
+        with torch.no_grad():
+            B, C, H, W = image_tensor.shape
+            cond = {"c_concat": [image_tensor], "c_crossattn": [model.get_learned_conditioning([prompt] * B)]}
+            un_cond = {"c_concat": image_tensor, "c_crossattn": [model.get_learned_conditioning([n_prompt] * B)]}
+            shape = (4, H // 8, W // 8)
+            model.control_scales = ([1.0] * 13)
+            samples, intermediates = sampler.sample(ddim_steps, B, shape, cond, verbose=False, eta=0,
+                                                    unconditional_guidance_scale=guidance_scale,
+                                                    unconditional_conditioning=un_cond)
+            result = model.decode_first_stage(samples)
+            return result  # [B, C, H, W], same format image_grid requires.
+
+
 # Misc
 dataset = MoveRectDataset()
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
-logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger, ckpt_callback])
+image_logger_callback = ImageLogger(batch_frequency=logger_freq)
+tensorboard_image_callback = TensorboardDiffusionImageSampler(ds=dataset)
+trainer = pl.Trainer(gpus=1, precision=32, callbacks=[image_logger_callback, ckpt_callback, tensorboard_image_callback])
 
 # Train!
 trainer.fit(model, dataloader)
